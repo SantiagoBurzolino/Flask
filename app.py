b1ee53b92b5823ff4e736b28f75400d07ac08359
SantiagoBurzolino <zemgogamer1@gmail.com>
2025-04-30 12:17:01 -0300

    agregue elmodif y el usuario

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
 from flask import Flask,url_for
+import sqlite3
 
 
 app = Flask(__name__)
@@ -52,3 +53,99 @@ def despedir():
 @app.route("/maquina")
 def decirte():
     return "<h2>maquina, que, sos!</h2>"
+
+
+db = None
+
+
+def dict_factory(cursor, row):
+  """Arma un diccionario con los valores de la fila."""
+  fields = [column[0] for column in cursor.description]
+  return {key: value for key, value in zip(fields, row)}
+
+
+def abrirConexion():
+   global db
+   db = sqlite3.connect("instance/datos.sqlite")
+   db.row_factory = dict_factory
+
+
+def cerrarConexion():
+   global db
+   db.close()
+   db = None
+
+
+@app.route("/test-db")
+def testDB():
+   abrirConexion()
+   cursor = db.cursor()
+   cursor.execute("SELECT COUNT(*) AS cant FROM usuarios; ")
+   res = cursor.fetchone()
+   registros = res["cant"]
+   cerrarConexion()
+   return f"Hay {registros} registros en la tabla usuarios"
+
+
+
+@app.route("/sqlite/delete/<int:id>")
+def testDelete(id):
+    abrirConexion()
+    #insertamos de forma segura
+    db.execute("DELETE FROM usuarios WHERE id=?", (id,))
+    db.commit()
+    cerrarConexion()
+    return f"Se borro el id {id} en la tabla usuarios."
+
+@app.route("/sqlite")
+def mostrarUsuarios():
+    abrirConexion()
+    cursor = db.execute("SELECT COUNT (usuario) AS cantidad FROM usuarios; ")
+    res = cursor.fetchone()
+    c = res['cantidad']
+    cerrarConexion()
+    return f"hay {c} registros en la tabla de usuarios"
+
+@app.route("/crearUsuario/<string:nombre>/<string:email>")
+def testCrear(nombre,email):
+    abrirConexion()
+    cursor = db.cursor()
+    consulta = "INSERT INTO usuarios(usuario, email) VALUES (?, ?);"
+    cursor.execute(consulta, (nombre, email))
+    db.commit()
+    cerrarConexion()
+    return f"Se Registro agregado ({nombre})"
+
+
+@app.route("/crearUsuario")
+def testCrearif():
+    nombre = "Pedro"
+    email = "Pedro@etec.uba.ar"
+    if nombre == "Pedro":
+        abrirConexion()
+        cursor = db.cursor()
+        consulta = "INSERT INTO usuarios(usuario, email) VALUES (?, ?);"
+        cursor.execute(consulta, (nombre, email))
+        db.commit()
+        cerrarConexion()  
+        return f"Registro agregado ({nombre})"
+    else:
+        return "Ya esta agregado el usuario"
+    
+
+@app.route("/sqlite/usuario/<int:id>")
+def seleccionindividual(id):
+    abrirConexion
+    cursor = db.execute("SELECT * FROM usuarios WHERE id=? ", (id,))
+
+
+
+@app.route("/sqlite/modif/<string:nombre>/<string:email>")
+def modifemail(nombre,email):
+    abrirConexion()
+    cursor = db.cursor()
+    consulta = "UPDATE usuarios SET email = ? WHERE usuario = ?;"
+    cursor.execute(consulta, (email, nombre))
+    db.commit()
+    cerrarConexion()
+    return f"Se modifico el email de usuario ({email})"
